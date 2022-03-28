615def32a60b2ab2d8678a8ebbe4b38e86e737c9
aki0000 <akihisa.fromm1@gmail.com>
2022-03-28 14:21:13 +0900

    init

---
+++ src/main/java/fish/payara/example/web/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package fish.payara.example.web;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
